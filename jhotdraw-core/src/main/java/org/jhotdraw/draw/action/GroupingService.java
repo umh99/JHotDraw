d5d80dc9b379dd1a99d01b83bee6c526ca388b5e
sido <allansido69@gmail.com>
2026-01-08 14:29:20 +0100

    Code Smell #4: Temporal Coupling

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/GroupingService.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/GroupingService.java
@@ -8,34 +8,84 @@ import org.jhotdraw.draw.figure.Figure;
 import java.util.Collection;
 import java.util.LinkedList;
 
-import java.util.Collection;
-
 public class GroupingService {
+
     public Collection<Figure> ungroup(DrawingView view, CompositeFigure group) {
-    // XXX - This code is redundant with UngroupAction
-        LinkedList<Figure> figures = new LinkedList<>(group.getChildren());
-        view.clearSelection();
-        group.basicRemoveAllChildren();
-        view.getDrawing().basicAddAll(view.getDrawing().indexOf(group), figures);
-        view.getDrawing().remove(group);
-        view.addToSelection(figures);
+        Drawing drawing = view.getDrawing();
+
+        LinkedList<Figure> figures = snapshotChildren(group);
+
+        beginSelectionUpdate(view);
+        detachChildrenFromGroup(group);
+        replaceGroupWithFigures(drawing, group, figures);
+        selectFigures(view, figures);
+
         return figures;
     }
 
     public void group(DrawingView view, CompositeFigure group, Collection<Figure> figures) {
-        Collection<Figure> sorted = view.getDrawing().sort(figures);
-        int index = view.getDrawing().indexOf(sorted.iterator().next());
-        view.getDrawing().basicRemoveAll(figures);
+        Drawing drawing = view.getDrawing();
+
+        Collection<Figure> sorted = sortForStableOrder(drawing, figures);
+        int insertionIndex = firstFigureIndex(drawing, sorted);
+
+        removeFiguresFromDrawing(drawing, figures);
+        beginSelectionUpdate(view);
+
+        insertGroupIntoDrawing(drawing, insertionIndex, group);
+        moveFiguresIntoGroup(group, sorted);
+
+        selectGroup(view, group);
+    }
+
+    private LinkedList<Figure> snapshotChildren(CompositeFigure group) {
+        return new LinkedList<>(group.getChildren());
+    }
+
+    private void beginSelectionUpdate(DrawingView view) {
         view.clearSelection();
-        view.getDrawing().add(index, group);
+    }
+
+    private void detachChildrenFromGroup(CompositeFigure group) {
+        group.basicRemoveAllChildren();
+    }
+
+    private void replaceGroupWithFigures(Drawing drawing, CompositeFigure group, Collection<Figure> figures) {
+        int index = drawing.indexOf(group);
+        drawing.basicAddAll(index, figures);
+        drawing.remove(group);
+    }
+
+    private void selectFigures(DrawingView view, Collection<Figure> figures) {
+        view.addToSelection(figures);
+    }
+
+    private Collection<Figure> sortForStableOrder(Drawing drawing, Collection<Figure> figures) {
+        return drawing.sort(figures);
+    }
+
+    private int firstFigureIndex(Drawing drawing, Collection<Figure> sorted) {
+        return drawing.indexOf(sorted.iterator().next());
+    }
+
+    private void removeFiguresFromDrawing(Drawing drawing, Collection<Figure> figures) {
+        drawing.basicRemoveAll(figures);
+    }
+
+    private void insertGroupIntoDrawing(Drawing drawing, int index, CompositeFigure group) {
+        drawing.add(index, group);
+    }
+
+    private void moveFiguresIntoGroup(CompositeFigure group, Collection<Figure> sorted) {
         group.willChange();
         for (Figure f : sorted) {
             f.willChange();
             group.basicAdd(f);
         }
         group.changed();
-        view.addToSelection(group);
     }
 
-
+    private void selectGroup(DrawingView view, CompositeFigure group) {
+        view.addToSelection(group);
+    }
 }
